c267963b4b5bece5741108d05a98443d5daefcfc
chaoqun <chaoqun@pku.edu.cn>
2018-09-26 22:45:20 +0800

    Network status

--- app/src/main/java/cn/pku/wuchaoqun/myweatherforecast/MainActivity.java
+++ app/src/main/java/cn/pku/wuchaoqun/myweatherforecast/MainActivity.java
@@ -2,6 +2,10 @@ package cn.pku.wuchaoqun.myweatherforecast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import cn.pku.wuchaoqun.util.NetUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORK_NONE) {
+            Log.d("myWeatherForecast", "网络ok！");
+            Toast.makeText(MainActivity.this, "网络ok!", Toast.LENGTH_SHORT).show();
+        }else {
+            Log.d("myWeatherForecast", "网络挂了！");
+            Toast.makeText(MainActivity.this, "网络未连接", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
